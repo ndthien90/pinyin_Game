a96519debac70716ed382208669f1bbf67a74e14
ndthien90 <ndthien@foxmail.com>
2025-07-29 13:45:01 +0700

    Initial commit

---
+++ _Run.bat
@@ -0,0 +1,10 @@
+@echo off
+:: Khởi động server HTTP của Python trong một cửa sổ mới và chạy ngầm
+start "" python -m http.server
+
+:: Đợi vài giây để server khởi động
+timeout /t 1 > nul
+
+:: Mở địa chỉ localhost:8000 trong trình duyệt mặc định
+start http://localhost:8000/pinyin_practice_app19_OK.html
+
